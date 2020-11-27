baff7f500f52e5ac8e3e80d95f314ac0ce4cde5b
Vainglorious <adilhooda@gmail.com>
2020-11-27 07:30:12 -0700

    Add files via upload

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 window.appConf = {
     author: 'React Drive CMS',
-    dashboardId: '1-on_GfmvaEcOk7HcWfKb8B6KFRv166RkLN2YmDEtDn4',
+    dashboardId: '1Lfrog_hdwxX6oCz46t_8NtLfROccH9qAAOmQHAcikqQ',
     sendContactMessageUrlId:
         'AKfycbyL4vW1UWs4mskuDjLoLmf1Hjan1rTLEca6i2Hi2H_4CtKUN84d',
     shortname: 'easydrivecms',
